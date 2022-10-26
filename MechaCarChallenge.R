b72ec18c4c4c168c9355d067fd6c9faa64ef53d3
montsecruz9 <104812189+montsecruz9@users.noreply.github.com>
2022-10-26 17:09:57 -0500

    T-Tests on suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,14 @@ library(dplyr)
 mecha_car <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) #read in dataset 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance,data=mecha_car) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance,data=mecha_car)) #enerate summary statistics
+
+suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) #read in dataset 
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+
+t.test(suspension_coil$PSI,mu = 1500)
+
+t.test(suspension_coil$PSI, subset=Manufacturing_Lot=="Lot1",mu = 1500)
+t.test(suspension_coil$PSI, subset=Manufacturing_Lot=="Lot2",mu = 1500)
+t.test(suspension_coil$PSI, subset=Manufacturing_Lot=="Lot3",mu = 1500)
